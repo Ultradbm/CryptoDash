d00b7035660e7bff574f192e6a1f9f5883f525e6
ultradbm <mmousa367@yahoo.com>
2020-09-07 01:18:14 -0400

    section 2 complete

--- src/App/App.js
+++ src/App/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import './App.css';
 import WelcomeMessage from "./WelcomeMessage"
+import styled, {css} from "styled-components";
 
 
+const MyButton = styled.div`
+  color: green;
+  ${props => props.primary && css`
+    color:palevioletred;
+  `}
+`
+
+const CoolButton = styled(MyButton)`
+  color:blue;
+  border-color:purple;
+`
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <WelcomeMessage/>
+        <MyButton>hi</MyButton>
+        <MyButton primary >hi</MyButton>
+        <CoolButton> hi</CoolButton>
       </header>
     </div>
   );
