6c62e5cc293ca2841997c8cad7749d091dd1eb19
ultradbm <mmousa367@yahoo.com>
2022-04-04 22:12:09 -0400

    Style AppBar

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -1,20 +1,46 @@
 /** @format */
 
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import React from "react";
 
 const Bar = styled.div`
   display: grid;
+  margin-bottom: 40px;
   grid-template-columns: 180px auto 100px 100px;
 `;
+
+const Logo = styled.div`
+  font-size: 1.5em;
+`;
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+const ControlButtonElem = styled.div`
+  cursor: pointer;
+  ${(props) =>
+    props.active &&
+    css`
+      text-shadow: 0px 0px 40px #03ff03;
+    `}
+`;
+
+const ControlButton = ({ name, active }) => {
+  return (
+    <ControlButtonElem active={active}>
+      {capitalizeFirstLetter(name)}
+    </ControlButtonElem>
+  );
+};
 
 export const AppBar = () => {
   return (
     <Bar>
-      <div>CryptoDash</div>
+      <Logo>CryptoDash</Logo>
       <div></div>
-      <div>Dashboard</div>
-      <div>Settings</div>
+      <ControlButton active name="dashboard" />
+      <ControlButton name="settings" />
     </Bar>
   );
 };
