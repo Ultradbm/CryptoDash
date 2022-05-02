43fc679d45076788effb59368d50697a05e2c5dc
ultradbm <mmousa367@yahoo.com>
2022-05-01 21:08:23 -0400

    Added Loading Content Component

---
+++ src/Shared/Content.js
@@ -0,0 +1,17 @@
+/** @format */
+
+import React from "react";
+import { AppContext } from "../App/AppProvider";
+
+export default function (props) {
+  return (
+    <AppContext.Consumer>
+      {({ coinList }) => {
+        if (!coinList) {
+          return <div>Loading Coins</div>;
+        }
+        return <div>{props.children}</div>;
+      }}
+    </AppContext.Consumer>
+  );
+}
